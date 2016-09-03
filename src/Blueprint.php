c2d91ce136ce613d1e9a9cd8fce8787e3d494857
Ahmad Rahimie <newrehmi@gmail.com>
2016-09-04 01:36:45 +0800

    Schema\Blueprint chained nullable fix

--- src/Blueprint.php
+++ src/Blueprint.php
@@ -24,4 +24,48 @@ class Blueprint extends \Illuminate\Database\Schema\Blueprint
 
 		// else probably compare and do some alteration
 	}
+
+	/**
+     * Add a "deleted at" timestamp for the table.
+     *
+     * @return \Illuminate\Support\Fluent
+     */
+	public function softDeletes()
+	{
+		$timestamp = $this->timestamp('deleted_at');
+
+		if(is_object($timestamp))
+			$timestamp->nullable();
+	}
+
+	/**
+	 * Adds the `remember_token` column to the table.
+	 *
+	 * @return \Illuminate\Support\Fluent
+	 */
+	public function rememberToken()
+	{
+		$object = $this->string('remember_token', 100);
+
+		if(is_object($object))
+			$object->nullable();
+	}
+
+	/**
+	 * Add nullable creation and update timestamps to the table.
+	 *
+	 * @return void
+	 */
+	public function nullableTimestamps()
+	{
+		$timestamp = $this->timestamp('created_at');
+
+		if(is_object($timestamp))
+			$timestamp->nullable();
+
+		$timestamp = $this->timestamp('updated_at');
+
+		if(is_object($timestamp))
+			$timestamp->nullable();
+	}
 }
